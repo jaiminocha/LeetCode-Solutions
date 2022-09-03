66a5560da2039dc45295306d99ecdf4d262b75fa
Jai Minocha <56990377+jaiminocha@users.noreply.github.com>
2022-09-03 22:50:47 +0530

    Time: 7 ms (38.82%), Space: 7 MB (96.71%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void solve(int n, int k, vector<int> &ans, int i, string output){
+    void solve(int n, int k, vector<int> &ans, string output){
         if (output.size() == n){
             ans.push_back(stoi(output));
             return;
@@ -8,25 +8,17 @@ public:
         
         for (int j = 0; j <= 9; j++){
             if (j == 0 && output.size() == 0) continue;
-            if (output.size() == 0){
-                output.push_back(j + '0');
-                solve(n, k, ans, i + 1, output);
-                output.pop_back();
-            }
-            else {
-                int diff = abs((output.back() - '0') - j);
-                if (diff == k){
-                    output.push_back(j + '0');
-                    solve(n, k, ans, i + 1, output);
-                    output.pop_back();
-                }
+            int diff = abs((output.back() - '0') - j);
+            if (output.size() == 0 || diff == k){
+                char ch = (j + '0');
+                solve(n, k, ans, output + ch);
             }
         }
     }
     vector<int> numsSameConsecDiff(int n, int k) {
         vector<int> ans;
         string output;
-        solve(n, k, ans, 0, output);
+        solve(n, k, ans, output);
         return ans;
     }
 };
