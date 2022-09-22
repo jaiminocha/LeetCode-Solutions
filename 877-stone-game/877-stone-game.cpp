2a142618a4e67fef487e3a432ab713aa4999498f
Jai Minocha <56990377+jaiminocha@users.noreply.github.com>
2022-09-22 16:11:52 +0530

    Time: 106 ms (8.20%), Space: 73.4 MB (6.76%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
+    int stoneDifference(vector<int> &p, int s, int e, vector<vector<int> > &dp){
+        if (s > e){
+            return 0;
+        }
+        if (dp[s][e] != INT_MIN){
+            return dp[s][e];
+        }
+        return dp[s][e] = max(p[s] - stoneDifference(p, s + 1, e, dp), p[e] - stoneDifference(p, s, e - 1, dp));
+    }
     bool stoneGame(vector<int>& piles) {
-        return true;
+        vector<vector<int> > dp(501, vector<int> (501, INT_MIN));
+        return stoneDifference(piles, 0, piles.size() - 1, dp) > 0;
     }
 };
