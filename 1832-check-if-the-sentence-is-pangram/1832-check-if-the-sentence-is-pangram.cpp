8c6108211b80204effcb405d1bf147887b77e7bb
Jai Minocha <56990377+jaiminocha@users.noreply.github.com>
2022-10-17 11:41:35 +0530

    Time: 3 ms (80.43%), Space: 6.7 MB (31.31%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        set<int> s;
+        for (auto letter : sentence){
+            s.insert(letter);
+        }
+        return s.size() == 26;
+    }
+};
