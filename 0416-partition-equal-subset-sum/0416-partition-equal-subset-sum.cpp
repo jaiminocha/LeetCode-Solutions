ec6d460533570643b9f6d0d15027c5a1ed7c6ddd
Jai Minocha <56990377+jaiminocha@users.noreply.github.com>
2022-11-19 18:10:24 +0530

    Time: 449 ms (50.36%), Space: 85.6 MB (20.17%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -8,7 +8,8 @@ public:
             return false;
         }
         if (dp[i][sum] != -1) return dp[i][sum];
-        bool op1 = solve(nums, n, req, sum + nums[i], i + 1, dp);
+        bool op1 = false;
+        if (sum + nums[i] <= req) op1 = solve(nums, n, req, sum + nums[i], i + 1, dp);
         bool op2 = solve(nums, n, req, sum, i + 1, dp);
         
         return dp[i][sum] = (op1 || op2);
@@ -18,7 +19,7 @@ public:
         int sum = 0;
         int totalSum = accumulate(begin(nums), end(nums), 0);
         if (totalSum % 2) return false;
-        vector<vector<int> > dp(n + 1, vector<int> (totalSum + 1, -1));
+        vector<vector<int> > dp(n + 1, vector<int> (totalSum/2 + 1, -1));
         return solve(nums, n, totalSum / 2, sum, 0, dp);
     }
 };
